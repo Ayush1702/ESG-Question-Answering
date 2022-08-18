b05d850170dea7afced4130cb4eb4c94399a7895
Ayush1702 <bhosleayush@gmail.com>
2022-08-18 13:35:06 +0530

    Streamlit App added

---
+++ ESG_QnA.py
@@ -0,0 +1,91 @@
+import re
+import copy
+import numpy as np
+from transformers import AutoConfig, AutoModel, QuestionAnsweringPipeline
+from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+import torch
+from numpy import ndarray
+from spacy.lang.en import English
+from transformers import pipeline
+import streamlit as st
+
+st.title("ESG Question Answering")
+st.text(f'torch version  is {torch.__version__}')
+st.write('You can paste your context text in the field below along with the question.')
+
+gpu_device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+HTML_WRAPPER = """<div style="
+                       overflow-x: auto; 
+                       border: 1px solid #e6e9ef; 
+                       border-radius: 0.25rem; 
+                       padding: 1rem; 
+                       margin-bottom: 
+                       2.5rem">
+                       {}</div>"""
+
+def set_bg_hack_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url(https://unsplash.com/photos/3ODJ3CeHlUo/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mjd8fGp1bmdsZXxlbnwwfHx8fDE2NjA3ODYzNDM&force=true);
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+set_bg_hack_url()
+
+@st.cache(allow_output_mutation=True)
+def esg_question_answering():
+    model_name = "roberta-base/"
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name, local_files_only=True)
+    return model
+
+# if 'context' not in st.session_state:
+    # st.session_state.context = None
+# if 'question_default' not in st.session_state:
+    # st.session_state.question_default = None
+
+with open("sample_context.txt", encoding="utf-8") as f:
+    contents = f.readlines()
+    sample_input_context = "".join(contents).lstrip().replace("\n"," ").replace("  ", " ")
+session_question_input_sample = "What is the emission reduction target aimed?"
+
+context_para_input = st.text_area("Enter Context Paragraph", sample_input_context, height = 400)
+question_input = st.text_area("Enter Question", session_question_input_sample, height = 45)
+st.session_state.context, st.session_state.question_default = context_para_input, question_input
+
+if st.button('Submit'):
+    context_input = st.session_state.context
+    question_input = st.session_state.question_default
+    with st.spinner('Loading Model'):
+        esg_model = esg_question_answering().to(gpu_device)
+    tokenizer_path = "roberta-base"
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, local_files_only=True)
+    question_answerer = pipeline("question-answering", model=esg_model, tokenizer=tokenizer, device=0)
+    result = question_answerer(question=question_input, context=context_input)
+    st.write(HTML_WRAPPER.format(result['answer']), unsafe_allow_html=True)
+
+# Make predictions with the model
+# to_predict = [
+    # {
+        # "context": context_para_input,
+        # "qas": [
+            # {
+                # "question": "What is the emission reduction mechanism or technology used here?",
+                # "id": "0",
+            # }
+        # ],
+    # }
+# ]
+
